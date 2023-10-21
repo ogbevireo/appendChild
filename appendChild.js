891a38a63c1d4e8008516bc373042de5e5200cf3
Ogbevire O., Obogrow <ogbevire@gmail.com>
2023-10-21 18:09:45 +0100

    appendChild to an element

---
+++ appendChild.js
@@ -0,0 +1,12 @@
+// WRITE YOUR CODE IN HERE:
+
+
+
+for (let i = 0; i < 100; i++) {
+  const button = document.createElement('button'); button.innerText = 'Hello World!';
+  button.style.backgroundColor = 'orange';
+  button.style.width = '71px';
+  button.style.borderRadius = '10px';
+  button.style.margin = '3px';
+  document.getElementById('container').appendChild(button);
+}
